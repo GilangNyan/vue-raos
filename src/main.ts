21951379fce9b06cbbbf7ba9fc24067604c885dd
Gilang Saeful Anwar <=>
2023-11-09 09:48:50 +0700

    Add multi language system with vue-i18n

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,27 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createI18n } from 'vue-i18n'
 
 import App from './App.vue'
 import router from './router'
+
+import en from '@/locale/en.json'
+import id from '@/locale/id.json'
+
+const i18n = createI18n({
+    locale: 'id',
+    messages: {
+        en: en,
+        id: id
+    }
+})
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(i18n)
 
 app.mount('#app')
