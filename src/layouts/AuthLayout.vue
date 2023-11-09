e45c6b356f39e5677c5f2f9dcb5a75d7879db931
Gilang Saeful Anwar <=>
2023-11-09 09:49:41 +0700

    Auth layout base done

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,4 +1,9 @@
 <template>
-  ini auth layout
-  <slot />
+  <div class="h-screen w-full bg-gradient-to-r from-dark-primary to-primary static">
+    <div class="absolute bg-white w-10/12 md:w-96 h-fit top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg p-8 shadow flex flex-col justify-center">
+      <img src="@/assets/images/firebase-logo.svg" alt="Logo" class="h-24 mb-8">
+      <slot />
+    </div>
+  </div>
+  
 </template>
