3523cb89c3db665e601c003a916a889af5b7c033
GilangNyan <sagilang@gmail.com>
2023-11-11 17:26:59 +0700

    Form Validation styling

--- src/components/input/InputRoundedWithIcon.vue
+++ src/components/input/InputRoundedWithIcon.vue
@@ -1,19 +1,25 @@
 <template>
-  <label for="username" class="relative block w-full">
+  <label for="username" class="block w-full">
     <span class="sr-only">{{ label }}</span>
-    <span class="absolute inset-y-0 left-0 flex items-center pl-3">
-      <slot />
+    <div class="relative">
+      <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+        <slot />
+      </span>
+      <input
+        :type="type"
+        :name="name"
+        :id="name"
+        class="rounded-full w-full border pl-10 pr-4 py-2 border-slate-400 placeholder-slate-400 focus:outline-none focus:ring-1"
+        :class="errorMessage != null ? 'focus:border-error focus:ring-error border-error' : 'focus:border-info focus:ring-info'"
+        :placeholder="label"
+        v-model="value"
+      />
+    </div>
+    <span class="text-error text-sm text-center justify-center w-full flex font-semibold">
+      {{ errorMessage }}
     </span>
-    <input
-      :type="type"
-      :name="name"
-      :id="name"
-      class="rounded-full w-full border pl-10 pr-4 py-2 border-slate-400 placeholder-slate-400 focus:outline-none focus:border-info focus:ring-info focus:ring-1"
-      :placeholder="label"
-      v-model="value"
-    />
-    <span>{{ errorMessage }}</span>
   </label>
+  
 </template>
 
 <script setup lang="ts">
